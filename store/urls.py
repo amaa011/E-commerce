6b600478e456b37162aa01bfefdac694571ed1be
amaa011 <112176819+amaa011@users.noreply.github.com>
2022-09-20 22:44:01 +0400

    the first commit

--- store/urls.py
+++ store/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
 
 	path('update_item/', views.updateItem, name="update_item"),
 	path('process_order/', views.processOrder, name="process_order"),
+	path('proudects/', views.processOrder, name="proudects")
 
 ]
